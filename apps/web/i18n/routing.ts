a409fa89c45667239eb3f0471c969c5ce3b7c31b
Trishantha <47658331+Trishantha@users.noreply.github.com>
2026-09-14 18:22:42 +0000

    restore locale-less default English routes

--- apps/web/i18n/routing.ts
+++ apps/web/i18n/routing.ts
@@ -4,7 +4,7 @@ import { createNavigation } from 'next-intl/navigation';
 export const routing = defineRouting({
   locales: ['en', 'si', 'ta'],
   defaultLocale: 'en',
-  localePrefix: 'always'
+  localePrefix: 'as-needed'
 });
 
 export const { Link, redirect, usePathname, useRouter, getPathname } = createNavigation(routing);
